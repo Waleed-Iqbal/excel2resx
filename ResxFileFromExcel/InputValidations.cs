30a2caceb2f5cc7060fdf4a43e6b4862d3d2429a
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-23 17:20:57 +0500

    Input validations classes added

---
+++ ResxFileFromExcel/InputValidations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxFileFromExcel
+{
+    public static class InputValidations
+    {
+        private static bool isCorrectInputFilePathGiven = false;
+        private static bool isCorrectOutputFolderPathGiven = false;
+
+        public static bool IsInputExcelFilePathValid()
+        {
+            isCorrectInputFilePathGiven = false;
+
+            return isCorrectInputFilePathGiven;
+        }
+
+
+        public static bool IsOutputFolderPathValid()
+        {
+            isCorrectOutputFolderPathGiven = false;
+
+            return isCorrectOutputFolderPathGiven;
+        }
+
+
+
+    }
+}
