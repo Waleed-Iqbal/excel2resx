f9e26b8e9109d0fb2f62ad4c6c8d5494dc414719
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-23 18:50:06 +0500

    Supported languages class added for better handling

---
+++ ResxFileFromExcel/SupportedLanguages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxFileFromExcel
+{
+    public static class SupportedLangues
+    {
+        public const string Greek = "el";
+        public const string Dutch = "nl";
+        public const string German = "de";
+        public const string French = "fr";
+        public const string English = "en";
+        public const string Spanish = "es";
+        public const string Swedish = "sv";
+        public const string Italian = "it";
+        public const string Norwegian = "nb";
+        public const string Portuguese = "pt";
+
+        //TODO: use the int logic here ... LOOK IT UP
+        public const bool IsGreekSelected = false;
+        public const bool IsDutchSelected = false;
+        public const bool IsGermanSelected = false;
+        public const bool IsFrenchSelected = false;
+        public const bool IsEnglishSelected = false;
+        public const bool IsSpanishSelected = false;
+        public const bool IsSwedishSelected = false;
+        public const bool IsItalianSelected = false;
+        public const bool IsNorwegianSelected = false;
+        public const bool IsPortugueseSelected = false;
+    }
+}
