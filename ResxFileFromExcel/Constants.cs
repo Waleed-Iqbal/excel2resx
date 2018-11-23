8154a01bb82965a83639ac6df171eb4945f489f0
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-23 17:59:50 +0500

    Constants file added to replace static strings and numbers

---
+++ ResxFileFromExcel/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxFileFromExcel
+{
+    public static class Constants
+    {
+
+        public const string INVALID_INPUT_FILE_PATH_ERROR = "Not a valid input excel file";
+        public const string INVALID_OUTPUT_DIRECTORY_PATH_ERROR = "Not a valid output folder";
+    }
+}
