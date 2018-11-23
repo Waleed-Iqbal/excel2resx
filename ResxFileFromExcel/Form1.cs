09badb1d4e2947c6f2cec1ecf32459f31620562a
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-23 17:22:19 +0500

    Input file path and output folder path dialogs added

--- ResxFileFromExcel/Form1.cs
+++ ResxFileFromExcel/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +13,13 @@ namespace ResxFileFromExcel
 {
     public partial class resxGenerator : Form
     {
-
-        public bool isCorrectInputPathGiven = false;
-        public bool isCorrectOutputPathGiven = false;
-
         public resxGenerator()
         {
             InitializeComponent();
+            btn_Generate_resx.Enabled = false;
         }
 
-        private void OutputResxFilePath_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void InputExcelFilePath_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Generate_resx_Click(object sender, EventArgs e)
+        private void resxGenerator_Load(object sender, EventArgs e)
         {
 
         }
@@ -39,13 +27,44 @@ namespace ResxFileFromExcel
         private void input_excel_path_TextChanged(object sender, EventArgs e)
         {
             // do validation here
-            isCorrectInputPathGiven = true;
         }
 
         private void output_resx_path_TextChanged(object sender, EventArgs e)
         {
             // do validation here
-            isCorrectOutputPathGiven = true;
+        }
+
+       
+
+        private void BrowseInputFile_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog selectFileDialog = new OpenFileDialog())
+            {
+                if (selectFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = selectFileDialog.FileName;
+                    tb_input_excel_path.Text = filePath;
+                }
+            }
+        }
+
+        private void BrowseForOutput_Click(object sender, EventArgs e)
+        {
+            using (var openFolderDialog = new FolderBrowserDialog())
+            { 
+                if (openFolderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string outputFolder = openFolderDialog.SelectedPath;
+                    tb_output_resx_path.Text = outputFolder;
+                }
+            }
+        }
+
+
+        private void Generate_resx_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
