5d641ec902af9c42cc7fdabaf3575802cb7f1233
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-26 13:51:14 +0500

    Complete language strings added

--- ResxFileFromExcel/Constants.cs
+++ ResxFileFromExcel/Constants.cs
@@ -12,5 +12,40 @@ namespace ResxFileFromExcel
         public const string INVALID_INPUT_FILE_PATH_ERROR = "Not a valid input excel file";
 
         public const string INVALID_OUTPUT_DIRECTORY_PATH_ERROR = "Not a valid output folder";
+
+        public const string GREEK = "Greek";
+        public const string DUTCH = "Dutch";
+        public const string GERMAN = "German";
+        public const string FRENCH = "French";
+        public const string ENGLISH = "English";
+        public const string SPANISH = "Spanish";
+        public const string SWEDISH = "Swedish";
+        public const string ITALIAN = "Italian";
+        public const string NORWEGIAN = "Norwegian";
+        public const string PORTUGUESE = "Portuguese";
+
+        public const string GREEK_SHORT_LOWER = "el";
+        public const string DUTCH_SHORT_LOWER = "nl";
+        public const string GERMAN_SHORT_LOWER = "de";
+        public const string FRENCH_SHORT_LOWER = "fr";
+        public const string ENGLISH_SHORT_LOWER = "en";
+        public const string SPANISH_SHORT_LOWER = "es";
+        public const string SWEDISH_SHORT_LOWER = "sv";
+        public const string ITALIAN_SHORT_LOWER = "it";
+        public const string NORWEGIAN_SHORT_LOWER = "nb";
+        public const string PORTUGUESE_SHORT_LOWER = "pt";
+
+
+        public const string GREEK_SHORT_UPPER = "GR";
+        public const string DUTCH_SHORT_UPPER = "BE";
+        public const string GERMAN_SHORT_UPPER = "DE";
+        public const string FRENCH_SHORT_UPPER = "FR";
+        public const string ENGLISH_SHORT_UPPER = "GB";
+        public const string SPANISH_SHORT_UPPER = "ES";
+        public const string SWEDISH_SHORT_UPPER = "SV";
+        public const string ITALIAN_SHORT_UPPER = "IT";
+        public const string NORWEGIAN_SHORT_UPPER = "NB";
+        public const string PORTUGUESE_SHORT_UPPER = "PT";
+
     }
 }
