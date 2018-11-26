73facc683ea555e6dbc90f60d8bbae7fcbf38069
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-26 11:56:35 +0500

    resx generator works with dummy data

--- ResxFileFromExcel/ResxGenerator.cs
+++ ResxFileFromExcel/ResxGenerator.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Resources;
 
 namespace ResxFileFromExcel
 {
     public static class ResxGenerator
     {
-        public static void Generate(string filePath)
+        public static void Generate(string inputFilePath, string outputDirectory)
         {
 
+            foreach (string key in SupportedLangues.localizationFileNames.Keys)
+            {
+                string fileName = SupportedLangues.localizationFileNames[key].FileName;
+                using (ResXResourceWriter generator = new ResXResourceWriter($"{outputDirectory}\\{fileName}.resx"))
+                {
+                    generator.AddResource("HeaderString3", "Year");
+                }
+            }
         }
     }
 }
