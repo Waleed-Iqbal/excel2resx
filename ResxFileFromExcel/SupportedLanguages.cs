e209784fdeef17f954eacee3fd84562fce93d27c
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-27 12:46:19 +0500

    Resource files' prefixes added

--- ResxFileFromExcel/SupportedLanguages.cs
+++ ResxFileFromExcel/SupportedLanguages.cs
@@ -16,16 +16,16 @@ namespace ResxFileFromExcel
 
     public static class OutputFileNames
     {
-        public const string Greek = Constants.GREEK_SHORT_LOWER + "-" + Constants.GREEK_SHORT_UPPER;
-        public const string Dutch = Constants.DUTCH_SHORT_LOWER + "-" + Constants.DUTCH_SHORT_UPPER;
-        public const string German = Constants.GERMAN_SHORT_LOWER + "-" + Constants.GERMAN_SHORT_UPPER;
-        public const string French = Constants.FRENCH_SHORT_LOWER + "-" + Constants.FRENCH_SHORT_UPPER;
-        public const string English = Constants.ENGLISH_SHORT_LOWER + "-" + Constants.ENGLISH_SHORT_UPPER;
-        public const string Spanish = Constants.SPANISH_SHORT_LOWER + "-" + Constants.SPANISH_SHORT_UPPER;
-        public const string Swedish = Constants.SWEDISH_SHORT_LOWER + "-" + Constants.SWEDISH_SHORT_UPPER;
-        public const string Italian = Constants.ITALIAN_SHORT_LOWER + "-" + Constants.ITALIAN_SHORT_UPPER;
-        public const string Norwegian = Constants.NORWEGIAN_SHORT_LOWER + "-" + Constants.NORWEGIAN_SHORT_UPPER;
-        public const string Portuguese = Constants.PORTUGUESE_SHORT_LOWER + "-" + Constants.PORTUGUESE_SHORT_UPPER;
+        public const string Greek = Constants.RESOURCE_FILE_PREFIX + Constants.GREEK_SHORT_LOWER + "-" + Constants.GREEK_SHORT_UPPER;
+        public const string Dutch = Constants.RESOURCE_FILE_PREFIX + Constants.DUTCH_SHORT_LOWER + "-" + Constants.DUTCH_SHORT_UPPER;
+        public const string German = Constants.RESOURCE_FILE_PREFIX + Constants.GERMAN_SHORT_LOWER + "-" + Constants.GERMAN_SHORT_UPPER;
+        public const string French = Constants.RESOURCE_FILE_PREFIX + Constants.FRENCH_SHORT_LOWER + "-" + Constants.FRENCH_SHORT_UPPER;
+        public const string English = Constants.RESOURCE_FILE_PREFIX + Constants.ENGLISH_SHORT_LOWER + "-" + Constants.ENGLISH_SHORT_UPPER;
+        public const string Spanish = Constants.RESOURCE_FILE_PREFIX + Constants.SPANISH_SHORT_LOWER + "-" + Constants.SPANISH_SHORT_UPPER;
+        public const string Swedish = Constants.RESOURCE_FILE_PREFIX + Constants.SWEDISH_SHORT_LOWER + "-" + Constants.SWEDISH_SHORT_UPPER;
+        public const string Italian = Constants.RESOURCE_FILE_PREFIX + Constants.ITALIAN_SHORT_LOWER + "-" + Constants.ITALIAN_SHORT_UPPER;
+        public const string Norwegian = Constants.RESOURCE_FILE_PREFIX + Constants.NORWEGIAN_SHORT_LOWER + "-" + Constants.NORWEGIAN_SHORT_UPPER;
+        public const string Portuguese = Constants.RESOURCE_FILE_PREFIX + Constants.PORTUGUESE_SHORT_LOWER + "-" + Constants.PORTUGUESE_SHORT_UPPER;
     }
 
     public static class SupportedLangues
