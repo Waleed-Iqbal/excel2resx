49b12105ac645d813d3a7903fb28ea9f085276e9
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-11-29 02:15:44 +0500

    removed some more unused using statements

--- ResxFileFromExcel/Form1.cs
+++ ResxFileFromExcel/Form1.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ResxFileFromExcel
 {
